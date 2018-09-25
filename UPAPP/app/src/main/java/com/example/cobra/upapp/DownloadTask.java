89c585e4ab9400feecc5f2239c23572b3ec88877
Hans <hans.haugusth@gmail.com>
2018-09-25 15:51:47 -0300

    que cosa

---
+++ UPAPP/app/src/main/java/com/example/cobra/upapp/DownloadTask.java
@@ -0,0 +1,11 @@
+package com.example.cobra.upapp;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+public class DownloadTask extends AsyncTask<String, Void, Bitmap> {
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        return null;
+    }
+}
--- UPAPP/app/src/main/java/com/example/cobra/upapp/Main2Activity.java
+++ UPAPP/app/src/main/java/com/example/cobra/upapp/Main2Activity.java
@@ -1,5 +1,6 @@
 package com.example.cobra.upapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 
@@ -38,5 +43,36 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
+        btnDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intent = new IntentIntegrator((Activity) getApplicationContext());
+                intent.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                intent.setPrompt("C�mera Scan");
+                intent.setCameraId(0);
+                intent.initiateScan();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null){
+            if (result.getContents() != null){
+
+            } else {
+                alert("Scan cancelado!");
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void alert(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
