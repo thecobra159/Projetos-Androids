4c917470d4209761c93ba6b6608cab2b24fd1cc9
Hans <hans.haugusth@gmail.com>
2018-09-28 13:46:45 -0300

    ta foda viu

--- UPAPPv3/app/src/main/java/com/example/thecobra/upappv3/TakePhoto.java
+++ UPAPPv3/app/src/main/java/com/example/thecobra/upappv3/TakePhoto.java
@@ -2,21 +2,31 @@ package com.example.thecobra.upappv3;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TakePhoto extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private Button btnPhoto;
     private ImageView imgThumbnail;
-    private String mCurrentPhotoPath;
+    private String mCurrentPhotoPath, timeStamp, imageFileName;
     private ProgressBar progressBar;
+    private File storageDir, image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +42,18 @@ public class TakePhoto extends AppCompatActivity {
             public void onClick(View view) {
                 Intent takePhoto = new Intent((MediaStore.ACTION_IMAGE_CAPTURE));
                 if (takePhoto.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePhoto, REQUEST_IMAGE_CAPTURE);
-//                    try {
-//                        File photoFile = FileUtils.createFile(getApplicationContext(), ".jpg");
-//                        mCurrentPhotoPath = photoFile.getAbsolutePath();
-//
-//                        Uri photoURI = FileProvider.getUriForFile(getApplicationContext(), "br.com.cobra.upapp.fileprovider", photoFile);
-//                        takePhoto.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (photoFile != null){
+                        Uri photoUri = FileProvider.getUriForFile(getApplicationContext(), "com.example.android.fileprovider", photoFile);
+                        takePhoto.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                        startActivityForResult(takePhoto, REQUEST_IMAGE_CAPTURE);
+                    }
                 }
             }
         });
@@ -54,9 +66,26 @@ public class TakePhoto extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap image = (Bitmap) extras.get("data");
             imgThumbnail.setImageBitmap(image);
-//            Bitmap imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
-//            imgThumbnail.setImageBitmap(imageBitmap);
+            galleryAddPic();
             AsyncTask<String, Void, Bitmap> upload = new UploadTask(getApplicationContext(), imgThumbnail, progressBar).execute(mCurrentPhotoPath);
         }
+    }
+
+    private File createImageFile() throws IOException {
+        timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        imageFileName = "JPEG_" + timeStamp + "_";
+        storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 }
